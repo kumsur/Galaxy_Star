04b2d7bcfa5f2d437ac1c833dc035754a9e66b64
hp <hp@DESKTOP-K9G7M92>
2022-12-27 15:41:25 +0530

    Generic Class from Eclipse

---
+++ TestNG_MK/src/gitHubMk/Generic.java
@@ -0,0 +1,16 @@
+package gitHubMk;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Generic
+{
+	
+	WebDriver driver;
+	PomSource pom;
+	
+	
+
+}
